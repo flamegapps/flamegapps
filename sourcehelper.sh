41de3eaa91d594713ab4f01fb189f662aa2e1a1c
ayandebnath <ayandebnath000@gmail.com>
2021-08-08 12:29:20 +0530

    improve git pull
    Signed-off-by: ayandebnath <ayandebnath000@gmail.com>

--- sourcehelper.sh
+++ sourcehelper.sh
@@ -19,7 +19,7 @@ BRANCH='master'
 if [ -d $DEST ]; then
   echo -e "\n${YELLOW}Pulling changes from APK repository $NC"
   cd $DEST
-  git pull
+  git pull origin $BRANCH
   if [ $? -gt 0 ]; then
     echo -e "--!$RED *** Unable to pull changes, please check your internet connection *** $NC"
     echo -e "--!$RED *** The script will now exit *** ${NC}"
